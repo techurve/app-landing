4d09b89b8fc6e8f810b39f716d2580e9cfa54466
Mark Bruderer <markbruderer123@gmail.com>
2023-12-12 14:30:32 +0100

    attempt fix.

--- app.vue
+++ app.vue
@@ -1,48 +1,7 @@
 <template>
-  <Header :links="navigation">
-    <template #logo>
-      <span>{{ appConfig.app_name }}</span>
-      <UBadge label="App" variant="subtle" class="mb-0.5" />
-    </template>
-  </Header>
-
-  <main class="min-h-[calc(100vh-var(4rem))]">
-    <NuxtPage />
-  </main>
-
-  <Footer :links="navigation">
-    <template #left>
-      Copyright © {{ new Date().getFullYear() }} {{ appConfig.app_name }}
-    </template>
-  </Footer>
+  <p>good</p>
 </template>
 
 <script setup lang="ts">
-import { type NavigationItem } from './types/navigation_item';
 
-const appConfig = useAppConfig();
-
-useHead({
-  title: 'App Landing Page',
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
-  htmlAttrs: {
-    lang: 'en'
-  }
-})
-
-useSeoMeta({
-  description: 'Mobile app landing page template. Start converting in minutes.'
-});
-
-const navigation: Array<NavigationItem> = [
-  { name: "Features", path: "features" },
-  { name: "Testimonials", path: "testimonials" },
-  { name: "Pricing", path: "pricing" },
-  { name: "FAQ", path: "faq" }
-];
 </script>
